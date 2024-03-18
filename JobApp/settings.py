18e2753963eb17d05f6cb4f1d19afa4b31b50842
Abel Tee <abeltemitayoade@gmail.com>
2024-03-18 15:17:18 +0100

    updated
    Signed-off-by: Abel Tee <abeltemitayoade@gmail.com>

--- JobApp/settings.py
+++ JobApp/settings.py
@@ -80,7 +80,7 @@ WSGI_APPLICATION = 'JobApp.wsgi.application'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-        {
+        'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
@@ -127,15 +127,9 @@ STATIC_URL = 'static/'
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 
-# if not DEBUG:    # Tell Django to copy static assets into a path called `staticfiles` (this is specific to Render)
-#     STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-#     # Enable the WhiteNoise storage backend, which compresses static files to reduce disk use
-#     # and renames the files with unique names for each version to support long-term caching
-#     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-
-STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, "static")
-]
+# STATICFILES_DIRS = [
+#     os.path.join(BASE_DIR, "static")
+# ]
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
